b529c022f5fea24c87c6c0f49ef66e6f948a040e
Dmitriy <unitofpeople@gmail.com>
2025-03-25 11:55:44 +0300

    Подключение миграций для бд

--- src/modules/database/entities/post.entity.ts
+++ src/modules/database/entities/post.entity.ts
@@ -38,10 +38,4 @@ export class Post {
     default: () => 'CURRENT_TIMESTAMP',
   })
   createdAt: Date;
-
-  @Column({
-    type: 'timestamp',
-    default: () => 'CURRENT_TIMESTAMP',
-  })
-  updatedAt: Date;
 }
